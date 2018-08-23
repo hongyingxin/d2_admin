6c96dd57d2d795ae2e987b0d9159b1dd62bd72df
hongyx <1104350235@qq.com>
2018-08-23 17:53:43 +0800

    加入弹窗功能

--- src/pages/page-demo/index.vue
+++ src/pages/page-demo/index.vue
@@ -10,11 +10,14 @@
         <el-table-column type="selection" width="55">
         </el-table-column>
 
-        <el-table-column label="日期" width="200">
-        <template slot-scope="scope">{{ scope.row.date }}</template>
+        <el-table-column type="index" width="70">
         </el-table-column>
 
-        <el-table-column prop="name" label="姓名" width="200">
+        <el-table-column prop="name" label="姓名" width="150">
+        </el-table-column>
+
+        <el-table-column label="日期" width="200">
+        <template slot-scope="scope">{{ scope.row.date }}</template>
         </el-table-column>
 
         <el-table-column prop="addr" label="地址" width="300">
@@ -23,20 +26,65 @@
         <el-table-column prop="email" label="邮箱" show-overflow-tooltip>
         </el-table-column>
 
+        <el-table-column label="操作" width="200">
+          <template scope="scope">
+              <el-button size="small" @click="handleEdit(scope.$index,scope.row)">编辑</el-button>
+              <el-button type="danger" @click="handleDel(scope.$index,scope.row)" size="small">删除</el-button>
+          </template>
+        </el-table-column>
+
     </el-table>
-    <div style="margin-top: 20px">
-        <el-button @click="toggleSelection([tableData3[1], tableData3[2]])">切换第二、第三行的选中状态</el-button>
-        <el-button @click="toggleSelection()">取消选择</el-button>
-    </div>
+
+    <!-- 编辑界面 -->
+    <el-dialog title="信息修改" :visible.sync="dialogFormVisible">
+      <el-form :model="form" label-position="right">
+
+        <el-form-item label="姓名" :label-width="formLabelWidth">
+          <el-input v-model="form.name" auto-complete="off"></el-input>
+        </el-form-item>
+        
+          <el-form-item label="日期" :label-width="formLabelWidth">
+              <el-date-picker type="date" placeholder="选择日期" v-model="form.date"></el-date-picker>
+          </el-form-item>
+
+
+        <el-form-item label="地址" :label-width="formLabelWidth">
+          <el-input v-model="form.addr" auto-complete="off"></el-input>
+        </el-form-item>
+
+        <el-form-item label="邮箱" :label-width="formLabelWidth">
+          <el-input v-model="form.email" auto-complete="off"></el-input>
+        </el-form-item>
+
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
+
     </d2-container>
 </template>
 <script>
 export default {
-        data() {
+    data() {
           return {
             tableData3: [],
+            formLabelWidth:'100px',
+            dialogFormVisible: false,
+            //编辑表单数据
+            form: {
+              name: '',
+              date: '',
+              addr: '',
+              email: ''
+            },
           }
         },
+
+    created(){
+      this.axiosGetRole();
+    },
 
     methods: {
 
@@ -61,13 +109,19 @@ export default {
           this.$refs.multipleTable.clearSelection();
         }
       },
+
       handleSelectionChange(val) {
         this.multipleSelection = val;
+      },
+      handleEdit:function(index,row){
+        this.dialogFormVisible = true;
+        this.form = Object.assign({},row);
       }
     },
+    
     mounted(){
-      this.axiosGetRole();
-    }
+      
+    },
 }
 </script>
 
